01be1d2ba5b96bef9ffab12a2ec044a05c6bd9df
Alina Tikhomirova <alinatmir@gmail.com>
2024-12-13 19:57:24 +0700

    Усложненная задача

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -23,9 +23,22 @@ class ViewController: UIViewController {
         }
     }
     
+    let dateFormatter = DateFormatter()
+    
+    private var historyText = "История изменений:" {
+        didSet {
+            historyTextView.text = historyText
+        }
+    }
+    
+    private func logHistoryValue(_ text: String) {
+        let date = Date()
+        let dateString = dateFormatter.string(from: date)
+        historyText += "\n\(dateString) \(text)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        minusButton.imageView?.contentMode = .scaleToFill
         
         historyBoxView.layer.cornerRadius = 16
         counterBoxView.layer.cornerRadius = 16
@@ -34,24 +47,42 @@ class ViewController: UIViewController {
         plusButton.backgroundColor = .clear
         minusButton.backgroundColor = .clear
         
+        plusButton.tintColor = .red
+        minusButton.tintColor = .blue
+        
         counterValue = 0
+        
+        historyText = "История изменений:"
+        
+        configureDateFormatter()
+        
+        historyTextView.isEditable = false
+        historyTextView.isSelectable = false
         // Do any additional setup after loading the view.
     }
     
     @IBAction func onMinusTap(_ sender: Any) {
         if counterValue > 0 {
             counterValue -= 1
+            logHistoryValue("значение изменено на -1")
         } else {
-            print("значение счётчика не должно падать ниже нуля")
+            logHistoryValue("попытка уменьшить значение счётчика ниже 0")
         }
     }
     
     @IBAction func onPlusTap(_ sender: Any) {
         counterValue += 1
+        logHistoryValue("значение изменено на +1")
     }
     
     @IBAction func onRepeatTap(_ sender: Any) {
         counterValue = 0
+        logHistoryValue("значение сброшено")
+    }
+    
+    private func configureDateFormatter() {
+        dateFormatter.locale = Locale(identifier: "ru_RU")
+        dateFormatter.dateFormat = "[d MMMM, yy HH:mm]:"
     }
 }
 
